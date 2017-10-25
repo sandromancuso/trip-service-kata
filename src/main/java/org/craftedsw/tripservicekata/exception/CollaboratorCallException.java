29bf9098aef3b2c98e9d15f9fd0b347201e64665
Fred <frederic.boisguerin@gmail.com>
2017-10-25 07:03:38 +0200

    Remove unused stuff

--- src/main/java/org/craftedsw/tripservicekata/exception/CollaboratorCallException.java
+++ src/main/java/org/craftedsw/tripservicekata/exception/CollaboratorCallException.java
@@ -4,22 +4,8 @@ public class CollaboratorCallException extends RuntimeException {
 
 	private static final long serialVersionUID = -4584041339906109902L;
 
-	public CollaboratorCallException() {
-		super();
-	}
-
-	public CollaboratorCallException(String message,
-			Throwable cause) {
-		super(message, cause);
-	}
-
 	public CollaboratorCallException(String message) {
 		super(message);
 	}
 
-	public CollaboratorCallException(Throwable cause) {
-		super(cause);
-	}
-
-	
 }
