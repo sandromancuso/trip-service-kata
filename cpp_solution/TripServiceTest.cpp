baea26ac1ed8f1e3670327ced7513cf31d0923a3
paveli <paveli@checkpoint.com>
2018-10-07 00:34:39 +0300

    reorder tests to reflect the code. clean up and reformating

--- cpp_solution/TripServiceTest.cpp
+++ cpp_solution/TripServiceTest.cpp
@@ -44,18 +44,17 @@ TEST_F(TripServiceTest, should_throw_in_no_user_logged_in)
 	EXPECT_THROW(tripService.GetTripsByUser(anyUser), UserNotLoggedInException);
 }
 
+TEST_F(TripServiceTest, should_return_trips_if_user_are_friend_with_logged_in_user)
+{
+	tripService.loggedInUser = &loggedUser;
+	anyUser.AddFriend(loggedUser);
+
+	EXPECT_EQ(2, tripService.GetTripsByUser(anyUser).size());
+}
 
 TEST_F(TripServiceTest, should_return_no_trips_if_users_are_not_friends)
 {
 	tripService.loggedInUser = &loggedUser;
 
 	EXPECT_EQ(0,tripService.GetTripsByUser(anyUser).size());
-}
-
-TEST_F(TripServiceTest, should_return_trips_if_user_are_friend_with_logged_in_user)
-{
-	tripService.loggedInUser = &loggedUser;
-	anyUser.AddFriend(loggedUser);
-
-	EXPECT_EQ(2,tripService.GetTripsByUser(anyUser).size());
 }
