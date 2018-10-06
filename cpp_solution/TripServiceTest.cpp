22f1202ff491d656bc9b1c74331461b4f4363412
paveli <paveli@checkpoint.com>
2018-10-06 23:50:05 +0300

    Add test should_return_no_trip_if_users_are_not_friends

--- cpp_solution/TripServiceTest.cpp
+++ cpp_solution/TripServiceTest.cpp
@@ -6,8 +6,11 @@ struct TripServiceTestable : TripService
 protected:
 	User* getLoggedInUser() override
 	{
-		return nullptr;	
+		return loggedInUser;	
 	}
+
+public:
+	User* loggedInUser = nullptr;
 };
 
 TEST(TripService, should_throw_in_no_user_logged_in) 
@@ -17,4 +20,15 @@ TEST(TripService, should_throw_in_no_user_logged_in)
 	User user{ 0 };
 
 	EXPECT_THROW(tripService.GetTripsByUser(user), UserNotLoggedInException);
+}
+
+
+TEST(TripService, should_return_no_trip_if_users_are_not_friends)
+{
+	TripServiceTestable tripService;
+
+	User user{ 0 };
+	User loggedUser{1};
+	tripService.loggedInUser = &loggedUser;
+	EXPECT_EQ(0,tripService.GetTripsByUser(user).size());
 }
