69cc133d8d0bee1fb23c7bff3de0cbce300e4d79
paveli <paveli@checkpoint.com>
2018-10-07 00:41:10 +0300

    inject loggedInUser as parameter

--- cpp_solution/TripServiceTest.cpp
+++ cpp_solution/TripServiceTest.cpp
@@ -41,7 +41,7 @@ TEST_F(TripServiceTest, should_throw_in_no_user_logged_in)
 {
 	tripService.loggedInUser = GUEST_USER;
 
-	EXPECT_THROW(tripService.GetTripsByUser(anyUser), UserNotLoggedInException);
+	EXPECT_THROW(tripService.GetTripsByUser(anyUser, GUEST_USER), UserNotLoggedInException);
 }
 
 TEST_F(TripServiceTest, should_return_trips_if_user_are_friend_with_logged_in_user)
@@ -49,12 +49,12 @@ TEST_F(TripServiceTest, should_return_trips_if_user_are_friend_with_logged_in_us
 	tripService.loggedInUser = &loggedUser;
 	anyUser.AddFriend(loggedUser);
 
-	EXPECT_EQ(2, tripService.GetTripsByUser(anyUser).size());
+	EXPECT_EQ(2, tripService.GetTripsByUser(anyUser, &loggedUser).size());
 }
 
 TEST_F(TripServiceTest, should_return_no_trips_if_users_are_not_friends)
 {
 	tripService.loggedInUser = &loggedUser;
 
-	EXPECT_EQ(0,tripService.GetTripsByUser(anyUser).size());
+	EXPECT_EQ(0,tripService.GetTripsByUser(anyUser, &loggedUser).size());
 }
