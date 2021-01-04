f8a795a396793978ddbfe2a4da2163f42eabe57b
guillaume de Touloluse <yohm31@gmail.com>
2021-01-04 22:38:30 +0100

    please

--- c#/TripServiceKata/TripServiceKata.Tests/TripServiceTest.cs
+++ c#/TripServiceKata/TripServiceKata.Tests/TripServiceTest.cs
@@ -1,6 +1,19 @@
-﻿namespace TripServiceKata.Tests
+﻿using NUnit.Framework;
+using TripServiceKata.Trip;
+using TripServiceKata.User;
+
+namespace TripServiceKata.Tests
 {
+    [TestFixture]
     public class TripServiceTest
     {
+        [Test]
+        public void HappyPath()
+        {
+            var sut = new TripService();
+            var user = new User.User();
+            var res = sut.GetTripsByUser(user);
+            Assert.That(res, Is.Not.Null);
+        }
     }
 }
