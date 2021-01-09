790eaffd1955c7dc2b55b9f72cef1508c54cb1ce
Irapuan <ibottosso@gmail.com>
2021-01-09 12:35:19 +0100

    Add netcore empty project

---
+++ netcore/TripServiceKata.Test/TripServiceKata.Test.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TripServiceKata.Test
+{
+    public class TripServiceKataTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1,1);
+        }
+    }
+}
