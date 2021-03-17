29d9deae71a68d40c9bd43c2b81f191e0251a634
Henrik Kirk <henrikkirk@gmail.com>
2021-03-17 09:36:39 +0100

    Initial commit for F# kata

---
+++ f#/TripServiceKata/TripServiceKata/Trip.fs
@@ -0,0 +1,3 @@
+namespace TripServiceKata.Trip
+
+type Trip = class end
---
+++ f#/TripServiceKata/TripServiceKata/TripService.fs
@@ -0,0 +1,24 @@
+﻿module TripServiceKata.Trip.TripService
+
+open TripServiceKata
+open TripServiceKata.User
+open TripServiceKata.Trip.TripServiceDAO
+
+exception UserNotLoggedInException
+
+let GetTripsByUser (user: User): Trip list =
+    let mutable tripList = []
+    let loggedInUser = UserSession.GetLoggedUser
+    let mutable friend = None
+    if (Option.isSome loggedInUser)
+    then
+        for f in user.Friends do
+            if f = Option.get loggedInUser
+            then
+                friend <- Some f
+        if (Option.isSome friend)
+        then
+            tripList <- FindTripsByUser (Option.get friend)
+        tripList
+    else
+        raise UserNotLoggedInException
