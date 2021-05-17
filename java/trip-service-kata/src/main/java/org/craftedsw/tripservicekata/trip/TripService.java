96ad934cb9cce3367bfcb6b348c20286eb328e3a
ruddy palvair <r.palvair@gmail.com>
2021-05-17 21:09:33 +0200

    tests: Throw exception when user not logged in

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -11,7 +11,7 @@ public class TripService {
 
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
 		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = UserSession.getInstance().getLoggedUser();
+		User loggedUser = getLoggedUser();
 		boolean isFriend = false;
 		if (loggedUser != null) {
 			for (User friend : user.getFriends()) {
@@ -28,5 +28,9 @@ public class TripService {
 			throw new UserNotLoggedInException();
 		}
 	}
-	
+
+	protected User getLoggedUser() {
+		return UserSession.getInstance().getLoggedUser();
+	}
+
 }
--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,25 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class TripServiceTest {
-	
+
+
+    @Test
+    public void should_throw_exception_when_user_not_logged_in() {
+        final TripService tripService = new TestableTripService();
+        final User user = new User();
+        assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(user));
+    }
+
+    private static class TestableTripService extends TripService {
+        @Override
+        protected User getLoggedUser() {
+            return null;
+        }
+    }
 }
