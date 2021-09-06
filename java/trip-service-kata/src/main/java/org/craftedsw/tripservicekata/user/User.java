5a32a1d777543453560b009046d796211dbc8937
ruddy palvair <r.palvair@gmail.com>
2021-09-06 20:58:06 +0200

    refactor: isFriendsWith(usr) method added to User class

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/user/User.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/user/User.java
@@ -1,29 +1,32 @@
 package org.craftedsw.tripservicekata.user;
 
+import org.craftedsw.tripservicekata.trip.Trip;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import org.craftedsw.tripservicekata.trip.Trip;
-
 public class User {
 
-	private List<Trip> trips = new ArrayList<Trip>();
-	private List<User> friends = new ArrayList<User>();
-	
-	public List<User> getFriends() {
-		return friends;
-	}
-	
-	public void addFriend(User user) {
-		friends.add(user);
-	}
+    private List<Trip> trips = new ArrayList<Trip>();
+    private List<User> friends = new ArrayList<User>();
 
-	public void addTrip(Trip trip) {
-		trips.add(trip);
-	}
-	
-	public List<Trip> trips() {
-		return trips;
-	}
+    public List<User> getFriends() {
+        return friends;
+    }
 
+    public void addFriend(User user) {
+        friends.add(user);
+    }
+
+    public void addTrip(Trip trip) {
+        trips.add(trip);
+    }
+
+    public List<Trip> trips() {
+        return trips;
+    }
+
+    public boolean isFriendsWith(User user) {
+        return friends.contains(user);
+    }
 }
--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
@@ -1,5 +1,32 @@
 package org.craftedsw.tripservicekata.user;
 
+import org.craftedsw.tripservicekata.trip.UserBuilder;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+
 public class UserTest {
 
+    private static final User BOB = UserBuilder.aUser().build();
+    private static final User PAUL = UserBuilder.aUser().build();
+
+    @Test
+    void should_return_false_when_users_are_not_friends() {
+        final User user = UserBuilder.aUser()
+                .friendsWith(BOB)
+                .build();
+
+        assertThat(user.isFriendsWith(PAUL), is(false));
+    }
+
+    @Test
+    void should_return_true_when_users_are_friends() {
+        final User user = UserBuilder.aUser()
+                .friendsWith(BOB, PAUL)
+                .build();
+
+        assertThat(user.isFriendsWith(PAUL), is(true));
+    }
 }
