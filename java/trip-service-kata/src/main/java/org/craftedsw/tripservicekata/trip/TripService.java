46d25c08cba1cb95d936fd4e4e3892a352652198
ruddy palvair <r.palvair@gmail.com>
2021-09-06 21:12:54 +0200

    refactor: TripService refactored

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -1,39 +1,33 @@
 package org.craftedsw.tripservicekata.trip;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.craftedsw.tripservicekata.user.UserSession;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TripService {
 
-	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = getLoggedUser();
-		boolean isFriend = false;
-		if (loggedUser != null) {
-			for (User friend : user.getFriends()) {
-				if (friend.equals(loggedUser)) {
-					isFriend = true;
-					break;
-				}
-			}
-			if (isFriend) {
-				tripList = getTripsList(user);
-			}
-			return tripList;
-		} else {
-			throw new UserNotLoggedInException();
-		}
-	}
+    public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
+        final User loggedUser = getLoggedUser();
+        if (loggedUser == null) {
+            throw new UserNotLoggedInException();
+        }
+        return user.isFriendsWith(loggedUser)
+                ? getTripsList(user)
+                : noTrips();
+    }
 
-	protected User getLoggedUser() {
-		return UserSession.getInstance().getLoggedUser();
-	}
+    private ArrayList<Trip> noTrips() {
+        return new ArrayList<>();
+    }
 
-	protected List<Trip> getTripsList(final User user) {
-		return TripDAO.findTripsByUser(user);
-	}
+    protected User getLoggedUser() {
+        return UserSession.getInstance().getLoggedUser();
+    }
+
+    protected List<Trip> getTripsList(final User user) {
+        return TripDAO.findTripsByUser(user);
+    }
 }
