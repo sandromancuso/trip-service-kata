dac51e2c80b41a8ebc6fab99f102c4d1df8728eb
bebright <william.shi@104.com.tw>
2022-04-19 18:16:22 +0800

    透過重構 測試時覆寫取得登入user，達到測試目的

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,6 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.user.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,8 +10,17 @@ public class TripServiceTest {
   @Test
   @DisplayName("使用者未登入丟出例外")
   void should_throw_exception_when_user_is_not_logged_in() throws Exception {
-    TripService tripService = new TripService();
+    TripService tripService = new TestableTripService();
     
     tripService.getTripsByUser(null);
+  }
+  
+  private class TestableTripService extends TripService {
+
+    @Override
+    protected User getLoggedUser() {
+      return null;
+    }
+    
   }
 }
