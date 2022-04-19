0ca07607805accf59f5d76fe607680f5cd9e4eac
bebright <william.shi@104.com.tw>
2022-04-19 18:04:02 +0800

    新增測試：使用者未登入丟出例外  (無法測試)

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,16 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class TripServiceTest {
 	
+  
+  @Test
+  @DisplayName("使用者未登入丟出例外")
+  void should_throw_exception_when_user_is_not_logged_in() throws Exception {
+    TripService tripService = new TripService();
+    
+    tripService.getTripsByUser(null);
+  }
 }
