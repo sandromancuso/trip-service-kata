cd4a814fe4211cafacb8496d05ac1a0a5a12ac3d
bebright <william.shi@104.com.tw>
2022-04-19 18:35:58 +0800

    完成 使用者未登入丟出例外 單元測試

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,8 @@
 package org.craftedsw.tripservicekata.trip;
 
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,9 +13,14 @@ public class TripServiceTest {
   @Test
   @DisplayName("使用者未登入丟出例外")
   void should_throw_exception_when_user_is_not_logged_in() throws Exception {
+    // arrange
     TripService tripService = new TestableTripService();
     
-    tripService.getTripsByUser(null);
+    // act
+    //assert
+    assertThatExceptionOfType(UserNotLoggedInException.class)
+      .isThrownBy(() -> tripService.getTripsByUser(null));
+    
   }
   
   private class TestableTripService extends TripService {
