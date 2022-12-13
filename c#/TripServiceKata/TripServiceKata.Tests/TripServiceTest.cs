9b9a171e603fc9bbb47d41df0541936e1859c8f6
Simon Gilquin <simon@gilquin.com>
2022-12-13 14:57:02 +0100

    updated to net6

--- c#/TripServiceKata/TripServiceKata.Tests/TripServiceTest.cs
+++ c#/TripServiceKata/TripServiceKata.Tests/TripServiceTest.cs
@@ -1,6 +1,13 @@
-﻿namespace TripServiceKata.Tests
+using NUnit.Framework;
+
+namespace TripServiceKata.Tests
 {
+    [TestFixture]
     public class TripServiceTest
     {
+        [Test]
+        public void Test()
+        {
+        }
     }
 }
