93ce2a827a0228a102d6ac3bb9da7cad396e4e38
Leandro Doctors <ldoctors@gmail.com>
2023-10-31 21:44:38 +0100

    JavaScript: fix: User class

--- javascript/src/User.js
+++ javascript/src/User.js
@@ -1,5 +1,14 @@
 "use strict";
 
-module.exports = class User {
+class User {
+	friends = [];
+	trips = [];
 
+	getFriends = () => this.friends;
+	getTrips = () => this.trips;
+
+	addFriend = (newFriend) => this.friends.push(newFriend);
+	addTrip = (newTrip) => this.trips.push(newTrip);
 }
+
+module.exports = User
